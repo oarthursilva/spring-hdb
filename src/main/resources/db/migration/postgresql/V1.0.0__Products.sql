2d683de3ee2e00690301681f009e6c5be2d160db
Arthur Silva <arthur.silva@blendit.com.br>
2021-12-31 18:13:20 -0300

    feat(mvn): adjust spring profiles (test,dev,cf)

--- src/main/resources/db/migration/postgresql/V1.0.0__Products.sql
+++ src/main/resources/db/migration/postgresql/V1.0.0__Products.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS "com.sap.jpa.hana.tables::products";
---DROP TABLE IF EXISTS product;
 
 CREATE TABLE "com.sap.jpa.hana.tables::products" (
---CREATE TABLE product (
     PRODUCT_UUID            varchar(100) UNIQUE NOT NULL,
     PRODUCT_NAME            varchar(500),
     STATUS                  varchar(50),
